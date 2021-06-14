b66d8e5fd1438713740b633cd496cfb0a62604a9
Andreas Ziegler <062.127@gmail.com>
2021-06-14 13:07:46 +0200

    Changes made by Thomas.

--- config.h
+++ config.h
@@ -14,21 +14,23 @@ static const Block blocks[] = {
 	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
+	{"",	"cpu",		2,	7},
+	{"",	"cpubars",	1,	18},
+	{"",	"memory",	5,	14},
 	/* {"",	"moonphase",	18000,	17}, */
 	{"",	"forecast",	18000,	5},
 	{"",	"mailbox",	180,	12},
-	{"",	"nettraf",	1,	16},
+	/* {"",	"nettraf",	1,	16}, */
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
+	{"",	"clock",	60,	1},
+	{"",	"kbselect",	5,	30},
 	{"",	"help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "|";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
